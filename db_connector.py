ee6f02a3a321fc35dfeed705a70d3a804085079c
BatimusPrime <primebatimus@gmail.com>
2018-02-08 01:08:21 -0800

    Really trying to just move a file here
    Really trying to just move a file here

--- db_connector.py
+++ db_connector.py
@@ -1,37 +1,62 @@
-'''
-
-Github: https://github.com/batimusprime/happybirthday/
-Date: January 30, 2018
-Project: Happy Birthday: An app that finds baseball player's birthdays
-Last Modified: 2/7/2018
-File Purpose: Push information to a MySQL databse
-
-'''
-
-############IMPORTS#################
-import pymysql
-
-
-############BEGIN MAIN SECTION#####
-
-#Call connect method and set database connection details
-conn = pymysql.connect(host='127.0.0.1', user='root',password='toor', db='playerDB')
-
-#Establish a cursor
-#TODO: Learn about cursor()
-a = conn.cursor()
-
-#Create prepared statement for ease of use & security best practice
-sql = 'SELECT * from players;'
-
-#Execute prepared statement with execute() method
-countrow = a.execute(sql)
-print(countrow)
-data = a.fetchall()
-print(data)# -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
-"""
-
+Github: https://github.com/batimusprime/happybirthday/ 
+Date: January 30, 2018 
+Project: Happy Birthday: An app that finds baseball player's birthdays 
+Last Modified: 2/8/2018 
+File Purpose: Push information to a MySQL databse 
+ 
+""" 
+############TODO#################### 
+ 
+#TODO: Create function to execute statements as arguments 
+#TODO: Implement some form of security procedures 
+ 
+############IMPORTS################# 
+ 
+import pymysql 
+ 
+ 
+############DEFINE VARIABLES####### 
+ 
+#TODO: Dynamic creation of variables 
+ 
+firstName = 'Chesty' 
+lastName = 'Puller' 
+birthdate = '1983-01-01' 
+ 
+############BEGIN MAIN SECTION##### 
+ 
+#Call connect method and set database connection details 
+conn = pymysql.connect(host='127.0.0.1', user='root',password='toor', db='playerDB') 
+ 
+#Establish a cursor 
+#TODO: Learn about cursor() 
+a = conn.cursor() 
+ 
+#Create prepared statement for ease of use & security best practice 
+selectAll = 'SELECT * from players;' 
+ 
+#Execute prepared statement with execute() method 
+countrow = a.execute(selectAll) 
+ 
+#printing object returns row number property 
+print("row count", countrow) 
+ 
+#can use fetchmany, but must establish a loop 
+data = a.fetchall() 
+ 
+#output the final data with simple loop 
+#TODO: Make this legible, using a template or (%s) 
+for rows in data: 
+    for name in rows: 
+        print(name) 
+     
+     
+     
+########INSERT INTO DB###### 
+     
+#create sql statement to execute inserting values into statement     
+a.execute("INSERT INTO players (firstName, lastNAme, birthdate) VALUES (%s,%s,%s)", 
+              (firstName, lastName,birthdate)) 
+#send command, like Git 
+conn.commit()
